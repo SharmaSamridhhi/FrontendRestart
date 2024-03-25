0edd5ba8301eb621601cc70e050adeb1357eb2cb
SharmaSamridhhi <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-03-25 16:24:32 +0000

    container config done

--- 01_basics/Practice.js
+++ 01_basics/Practice.js
@@ -10,3 +10,28 @@
 // console.log(null > 0);
 // console.log(null == 0);
 // console.log(null === 0);
+
+const myArr = new Array(1,2,3,4,5);
+// myArr.push(5)
+// myArr.push(6)
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr)
+// console.log(myArr.includes(1));
+// console.log(myArr.indexOf(1));
+
+// const newArr = myArr.join();
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof myArr);
+// console.log(typeof newArr);
+
+console.log("A original " , myArr)
+const myn1 = myArr.slice(1,3);
+console.log("B slice " , myn1)
+const myn2 = myArr.splice(1,3);
+console.log("A original after splice" , myArr)
+console.log("C splice " , myn2)
+
+
+
