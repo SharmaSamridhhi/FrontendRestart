a0a6bdadcdf8c9dba4faa87f7598389fedee4bf3
SharmaSamridhhi <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-03-25 11:06:07 +0000

    container config done

--- 01_basics/Practice.js
+++ 01_basics/Practice.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == "2");
+// console.log(2 != 1);
+// console.log(2 === "2");
+
+// console.log(null >= 0);
+// console.log(null <= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null === 0);
