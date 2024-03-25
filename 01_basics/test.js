cf108b0295dbd0a8f57e22b7aba2b0fe57dde393
SharmaSamridhhi <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-03-25 08:40:03 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
-console.log("Samridhhi")
+let score = true;
+// console.log("score is " + score + " type of " + typeof(score));
+
+let Value2 = Number(score);
+// console.log("Value2 is " + Value2 + " type of " + typeof(Value2));
+
+
+let isLoggedIn = 1;
+let isLoggedInbool = Boolean(isLoggedIn);
+console.log("isLoggedInbool is " + isLoggedInbool + " type of " + typeof(isLoggedInbool));
