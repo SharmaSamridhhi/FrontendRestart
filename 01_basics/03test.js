655b1e895840ffa52f2c23cec2d01954176e17e2
Samridhhi Sharma <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-03-26 16:07:34 +0000

    container config done

---
+++ 01_basics/03test.js
@@ -0,0 +1,16 @@
+// const marvel_heroes = ["Iron man","thor","spider man"];
+// const dc_heroes = ["superman","batman","flash"];
+// marvel_heroes.push(dc_heroes);
+// console.log(marvel_heroes[3][2]);
+// const newArr = [...marvel_heroes , ...dc_heroes];
+// console.log(newArr);
+
+// const arr = [1,2,3,4,5,[1,2,3,[6,7,8,9]],[22,45,78]];
+// const newarr = arr.flat(Infinity);
+// console.log(arr);
+// console.log(newarr);
+
+
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
