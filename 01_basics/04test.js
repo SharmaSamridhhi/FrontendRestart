543970bcffc281ec50d1db7abd8ae187cd1c9f47
Samridhhi Sharma <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-03-30 15:08:53 +0000

    container config done

--- 01_basics/04test.js
+++ 01_basics/04test.js
@@ -28,7 +28,16 @@
 //     }
 // }
 // console.log(regularUser.fullName.userFullName.firstName , regularUser.fullName.userFullName.lastName);
-
+const obj1 = {
+    1 : "a",
+    2 : "b"
+}
+const obj2 = {
+    3 : "c",
+    4 : "d"
+}
+const obj3 = Object.assign({} , obj1 , obj2)
+console.log(obj3);
 
 
 
