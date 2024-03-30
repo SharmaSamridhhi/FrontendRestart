70aa9c4268cb07f680a65b4f05b3b09946d71622
Samridhhi Sharma <133076418+SharmaSamridhhi@users.noreply.github.com>
2024-03-30 13:59:40 +0000

    container config done

--- 01_basics/04test.js
+++ 01_basics/04test.js
@@ -1,16 +1,22 @@
-const mySym = Symbol("key1");
-const mySym2 = Symbol("key1");
-const JsUser = {
-    name : "Hitesh",
-    [mySym] : "mykey1",
-    "full name" : "Hitesh Choudhary",
-    age : 18,
-    location : "Jaipur",
-    email : "hitesh@google.com",
-    isLoggedIn : false
-}
+// const JsUser = {
+//     name : "Hitesh",
+//     "full name" : "Hitesh Choudhary",
+//     age : 18,
+//     location : "Jaipur",
+//     email : "hitesh@google.com",
+//     isLoggedIn : false
+// }
+// JsUser.greetings = function(){
+//     console.log("Hello from JsUser");
+// }
+// JsUser.greetingsTwo = function(){
+//     console.log(`Hello to ${this["full name"]}`);
+// }
+// console.log(JsUser.greetingsTwo());
 
-console.log(typeof JsUser[mySym]);
-console.log(typeof mySym2);
+
+
+
+
 
 
